0555d60e6e0796acb75c87c8aed642d2995e02c3
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-03-01 20:22:37 +0700

    Update filter options movie based on current selected genre

--- scripts/booking-ticket.js
+++ scripts/booking-ticket.js
@@ -7,14 +7,6 @@ let movieSelectByGenre = db_movies.filter(movie => movie.genre == "action");
 $(document).ready(() => {
   // Show options of movie which "action" genre as default
   showMovieOptions();
-});
-
-let showMovieOptions = () => {
-  $(".movie-options").empty();
-
-  movieSelectByGenre.forEach(movie => {
-    $(".movie-options").append(createOption(movie.title));
-  });
 
   // Every options genre changed, update options of movie
   // Just show options movie from current selected genre
@@ -31,6 +23,14 @@ let showMovieOptions = () => {
 
     // Show options of movie which current selected genre
     showMovieOptions();
+  });
+});
+
+let showMovieOptions = () => {
+  $(".movie-options").empty();
+
+  movieSelectByGenre.forEach(movie => {
+    $(".movie-options").append(createOption(movie.title));
   });
 };
 
