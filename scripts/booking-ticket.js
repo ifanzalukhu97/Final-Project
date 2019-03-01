0a9014ae4e0d5b8945ce0dba83fd1a68bec46b10
ifanzalukhu97 <ivan.zalukhu97@gmail.com>
2019-03-01 20:42:04 +0700

    Add confirmation booking ticket details modal

--- scripts/booking-ticket.js
+++ scripts/booking-ticket.js
@@ -4,6 +4,8 @@ let db_movies = movies;
 // by default, get all movies with "action" genre
 let movieSelectByGenre = db_movies.filter(movie => movie.genre == "action");
 
+let detailsBookingTicket = {};
+
 $(document).ready(() => {
   // Show options of movie which "action" genre as default
   showMovieOptions();
@@ -42,6 +44,25 @@ $(".ticket-movie-qty").change(() => {
   setTotalPayment();
 });
 
+// Update total payment every ticket qty changed
+$(".ticket-movie-qty").change(() => {
+  setTotalPayment();
+});
+
+$(".btn-booking-ticket").click(() => {
+  getDetailBookingTicket();
+
+  $(".modal-detail-ticket").empty();
+
+  createFormGroup("Username", detailsBookingTicket.username);
+  createFormGroup("Email", detailsBookingTicket.email);
+  createFormGroup("Price", detailsBookingTicket.price);
+  createFormGroup("Qty", detailsBookingTicket.qty);
+  createFormGroup("Total", detailsBookingTicket.total_payment);
+
+  $(".modal-detail-poster").attr("src", detailsBookingTicket.poster_path);
+});
+
 let showMovieOptions = () => {
   $(".movie-options").empty();
 
@@ -73,8 +94,42 @@ let setTotalPayment = () => {
   $(".total-payment").val(ticketMovieQty * selectedMoviePrice);
 };
 
+let getDetailBookingTicket = () => {
+  let movieTitleSelected = $(".movie-options")
+    .find(":selected")
+    .val();
+
+  let movieSelected = movieSelectByGenre.filter(
+    movie => movie.title == movieTitleSelected
+  );
+
+  detailsBookingTicket.username = $(".username").val();
+  detailsBookingTicket.email = $(".email").val();
+  detailsBookingTicket.price = movieSelected[0].price;
+  detailsBookingTicket.qty = $(".ticket-movie-qty").val();
+  detailsBookingTicket.total_payment = $(".total-payment").val();
+  detailsBookingTicket.poster_path = movieSelected[0].poster_path;
+};
+
 // ======= HTML Templating ========
 let createOption = movieTitle => {
   return `<option>${movieTitle}</option>`;
+};
+
+let createFormGroup = (label, value) => {
+  let formGroup = `<div class="form-group row">
+  <label class="col-sm-4 col-form-label">
+    ${label}
+  </label>
+  <div class="col-sm-8">
+    <input
+      readonly
+      class="form-control-plaintext text-white"
+      value="${value}"
+    />
+  </div>
+</div>`;
+
+  $(".modal-detail-ticket").append(formGroup);
 };
 // ======= End HTML Templating ========
